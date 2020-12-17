e733aab6490d8b2164cb0a36cc530f4e2d23d843
Elliot <ebossi10@stuy.edu>
2020-12-16 22:00:20 -0500

    worked first time, very happy

--- Sorts.java
+++ Sorts.java
@@ -3,5 +3,24 @@ public class Sorts{
   *Upon completion, the elements of the array will be in increasing order.
   *@param data  the elements to be sorted.
   */
-  public static void bubbleSort(int[] data){  }
+  //do not print/return
+  public static void bubbleSort(int[] data){
+    int i = 0;
+    int p = 0;
+    int temp = 0;
+    boolean x = false;
+    while (p < data.length){
+      i = 0;
+      while (i < data.length-1){
+        temp = 0;
+        if (data[i] > data[i+1]){
+          temp = data[i];
+          data[i] = data[i+1];
+          data[i+1] = temp;
+        }
+        i += 1;
+      }
+      p += 1;
+    }
+  }
 }
